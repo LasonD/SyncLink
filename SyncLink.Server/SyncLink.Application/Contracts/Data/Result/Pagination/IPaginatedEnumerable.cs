e2b97961c52e411276c0a8330daeebe216851e69
Oleksandr Barylo <sbarylo20023@gmail.com>
2023-03-04 23:09:45 +0200

    Add paginated enumerable

---
+++ SyncLink.Server/SyncLink.Application/Contracts/Data/Result/Pagination/IPaginatedEnumerable.cs
@@ -0,0 +1,14 @@
+﻿namespace SyncLink.Application.Contracts.Data.Result.Pagination;
+
+public interface IPaginatedEnumerable<out T> : IEnumerable<T>
+{
+    int Page { get; }
+    int? NextPage { get; }
+    int? PreviousPage { get; }
+    int LastPage { get; }
+    int ItemCount { get; }
+    int PageSize { get; }
+    int PageCount { get; }
+    bool HasPreviousPage { get; }
+    bool HasNextPage { get; }
+}
---
+++ SyncLink.Server/SyncLink.Application/Contracts/Data/Result/Pagination/PaginatedCollection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+namespace SyncLink.Application.Contracts.Data.Result.Pagination;
+
+public class PaginatedEnumerable<T> : IPaginatedEnumerable<T>
+{
+    private readonly IEnumerable<T> _entities;
+
+    public PaginatedEnumerable(IEnumerable<T> entities, int itemCount, int page, int pageSize)
+    {
+        _entities = entities;
+        ItemCount = itemCount;
+        Page = page;
+        PageSize = pageSize;
+        PageCount = pageSize > 0 ? (int)Math.Ceiling(itemCount / (double)pageSize) : 0;
+        LastPage = PageCount;
+    }
+
+    public int Page { get; }
+    public int? NextPage => HasNextPage ? Page + 1 : null;
+    public int? PreviousPage => HasPreviousPage ? Page - 1 : null;
+    public int LastPage { get; }
+    public int ItemCount { get; }
+    public int PageSize { get; }
+    public int PageCount { get; }
+    public bool HasPreviousPage => Page > 1;
+    public bool HasNextPage => Page < LastPage;
+
+    public IEnumerator<T> GetEnumerator() => _entities.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
