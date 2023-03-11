ab2642de049a011fc621bd2441d956459a6bd5b4
Oleksandr Barylo <sbarylo20023@gmail.com>
2023-03-11 19:25:43 +0200

    Add common helpers

--- SyncLink.Server/SyncLink.Application/Domain/Message.cs
+++ SyncLink.Server/SyncLink.Application/Domain/Message.cs
@@ -1,8 +1,22 @@
-﻿namespace SyncLink.Application.Domain;
+﻿using SyncLink.Common.Validation;
+
+namespace SyncLink.Application.Domain;
 
 public class Message : EntityBase
 {
+    protected Message() { }
+
+    public Message(User sender, Room room, string text)
+    {
+        sender.ThrowIfNull(nameof(sender));
+        room.ThrowIfNull(nameof(room));
+
+        Text = text;
+    }
+
     public bool IsEdited { get; private set; } = false;
+
+    public string Text { get; private set; } = null!;
 
     public int SenderId { get; private set; } = 0;
     public User Sender { get; private set; } = null!;
---
+++ SyncLink.Server/SyncLink.Common/Validation/ValidationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using SyncLink.Common.Helpers;
+
+namespace SyncLink.Common.Validation;
+
+public static class ValidationExtensions
+{
+    public static void ThrowIfNull(this object value, string? parameterName = null)
+    {
+        if (value == null) throw new ArgumentNullException($"{parameterName ?? "Parameter"} cannot be null.");
+    }
+
+    public static void ThrowIfNullOrWhiteSpace(this string value, string? parameterName = null)
+    {
+        if (value.IsNullOrWhiteSpace()) throw new ArgumentException($"{parameterName ?? "Parameter"} cannot be null or white space.");
+    }
+}
