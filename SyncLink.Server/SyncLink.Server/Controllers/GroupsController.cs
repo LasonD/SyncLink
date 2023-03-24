7d9113b1369d6f62237f7b62afcb2e6d27af820e
Oleksandr Barylo <sbarylo20023@gmail.com>
2023-03-24 21:01:31 +0200

    Add search action

--- SyncLink.Server/SyncLink.Server/Controllers/GroupsController.cs
+++ SyncLink.Server/SyncLink.Server/Controllers/GroupsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SyncLink.Application.UseCases.Queries.GetById.Group;
+using SyncLink.Application.UseCases.Queries.SearchGroups;
 
 namespace SyncLink.Server.Controllers;
 
@@ -21,6 +22,16 @@ public class GroupsController : ControllerBase
     public async Task<IActionResult> GetById(int id, CancellationToken cancellationToken)
     {
         var query = new GetGroupById.Query { Id = id };
+
+        var result = await _mediator.Send(query, cancellationToken);
+
+        return Ok(result);
+    }
+
+    [HttpGet("search")]
+    public async Task<IActionResult> Search([FromQuery] string? searchQuery = null, CancellationToken cancellationToken = default)
+    {
+        var query = new SearchGroups.Query(searchQuery);
 
         var result = await _mediator.Send(query, cancellationToken);
 
