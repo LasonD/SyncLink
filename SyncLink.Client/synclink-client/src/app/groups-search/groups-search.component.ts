df2818a7e48022939e474f041e7eafe400b75a92
Oleksandr Barylo <sbarylo20023@gmail.com>
2023-03-24 22:31:44 +0200

    Init groups search form

---
+++ SyncLink.Client/synclink-client/src/app/groups-search/groups-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-groups-search',
+  templateUrl: './groups-search.component.html',
+  styleUrls: ['./groups-search.component.scss']
+})
+export class GroupsSearchComponent {
+
+}
