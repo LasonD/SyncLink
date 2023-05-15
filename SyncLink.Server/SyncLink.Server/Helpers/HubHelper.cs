ada835ba0de2e440c94c6364868ade482d9e1e49
Oleksandr Barylo <sbarylo20023@gmail.com>
2023-05-15 10:53:59 +0300

    Adjust

---
+++ SyncLink.Server/SyncLink.Server/Helpers/HubHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace SyncLink.Server.Helpers;
+
+public static class HubHelper
+{
+    public static string GetGroupNameForGroupId(int groupId)
+    {
+        return $"Group_{groupId}";
+    }
+}
